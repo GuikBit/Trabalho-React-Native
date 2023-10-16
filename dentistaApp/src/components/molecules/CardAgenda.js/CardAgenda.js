37949c81144ab1935fff25426f125c9d46435cf2
matheusFabri <matheus.fabri@viannasempre.com.br>
2023-10-16 20:22:30 -0300

    card agenda

--- dentistaApp/src/components/molecules/CardAgenda.js/CardAgenda.js
+++ dentistaApp/src/components/molecules/CardAgenda.js/CardAgenda.js
@@ -1,14 +1,21 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import StyledText from "../../atoms/StyledText";
 
 const CardAgenda = () => {
   return (
     <View>
-      <Text>CardAgenda</Text>
+      <View>
+        <StyledText style={styles.timeText}>10h50</StyledText>
+      </View>
     </View>
   );
 };
 
 export default CardAgenda;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  timeText: {
+    fontSize: 20,
+  },
+});
--- dentistaApp/src/view/Agendamento/Agendamento.js
+++ dentistaApp/src/view/Agendamento/Agendamento.js
@@ -1,14 +1,24 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import CardAgenda from "../../components/molecules/CardAgenda.js/CardAgenda";
 
 const Agendamento = () => {
   return (
-    <View>
-      <Text>Agendamento</Text>
+    <View style={styles.outerContainer}>
+      <CardAgenda />
+      <CardAgenda />
+      <CardAgenda />
+      <CardAgenda />
+      <CardAgenda />
     </View>
   );
 };
 
 export default Agendamento;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  outerContainer: {
+    flex: 1,
+    padding: 15,
+  },
+});
